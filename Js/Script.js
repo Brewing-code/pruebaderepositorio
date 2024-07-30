434f0167f7efc53d71c34cdea4388af67dc8b345
Franco Marolla <franco@MacBook-Pro.local>
2024-07-30 11:19:12 -0300

    Agregando funciones a mi codigo

--- Js/Script.js
+++ Js/Script.js
@@ -1,2 +1,13 @@
-let nombre = "Franco";
+let nombre = prompt('Ingresa tu nombre');
+let apellido = prompt('Ingresa tu apellido')
+let nombre_completo =(nombre+""+apellido)
+let edad = prompt('Ingresa tu edad');
+    if (edad < 18) {
+      alert('Para acceder a este sitio tienes que tener más de 18 años');
+    } else {
+      alert('Bienvenido al sitio web.');
+    }
+    
+
+
 console.log(nombre);
