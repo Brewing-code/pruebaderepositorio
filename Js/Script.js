4e9be9fe015770ca08a225595eebaf8922b296db
Franco Marolla <franco@MacBook-Pro.local>
2024-07-30 11:22:37 -0300

    Correcion de codigo

--- Js/Script.js
+++ Js/Script.js
@@ -1,13 +1,11 @@
 let nombre = prompt('Ingresa tu nombre');
-let apellido = prompt('Ingresa tu apellido')
-let nombre_completo =(nombre+""+apellido)
+let apellido = prompt('Ingresa tu apellido');
+let nombre_completo =(nombre+" "+apellido);
 let edad = prompt('Ingresa tu edad');
     if (edad < 18) {
       alert('Para acceder a este sitio tienes que tener más de 18 años');
     } else {
       alert('Bienvenido al sitio web.');
     }
-    
-
-
-console.log(nombre);
+console.log(nombre_completo);
+console.log(edad);
